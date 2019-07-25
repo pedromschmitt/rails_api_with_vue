aa18671a2b7ee93c50b10a749b01c2c4764ee54d
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-07-25 15:52:02 -0300

    fix routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   post 'refresh', controller: :refresh, action: :create
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
-  delete 'signin', crontroller: :signin, action: :destroy
+  delete 'signin', controller: :signin, action: :destroy
 
 end
